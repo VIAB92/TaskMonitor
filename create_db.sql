97d6e05aa155a3654842dca3b9f54b3c52da4fc3
VIAB92 <victor_man@inbox.ru>
2013-11-13 12:06:17 +0200

    Commit 2 - with SQL reports

---
+++ create_db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE reporter(
+  monitor VARCHAR(10) NOT NULL,
+  active NUMBER(6) NOT NULL,
+  complete NUMBER(6) NOT NULL,
+  taskCount NUMBER(6) NOT NULL,
+  isShutdown VARCHAR(3) NOT NULL,
+  isTerminated VARCHAR(3) NOT NULL
+  );
+/*
+Table created.
+*/
